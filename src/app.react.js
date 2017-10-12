f2ffc839449fb141d22d92859d96f714e3041a7d
DennisdeBest <dennis@debest.fr>
2017-10-12 14:58:15 +0200

    file structure

---
+++ src/app.react.js
@@ -0,0 +1,16 @@
+class TweetBox extends React.Component {
+
+  render() {
+    return (
+      <div>
+     coucou
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(
+  <TweetBox/>,
+  document.getElementById('react-app')
+);
